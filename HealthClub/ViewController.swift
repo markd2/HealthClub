d338f2153e0ee689a2d9525d0b0f89fbe235dc98
markd <markd@borkware.com>
2018-05-09 09:57:50 -0400

    Adds queue-safe messaging bottleneck

--- HealthClub/ViewController.swift
+++ HealthClub/ViewController.swift
@@ -14,6 +14,12 @@ class ViewController: UIViewController {
     @IBOutlet var messageLabel: UILabel!
     var workouts: [HKWorkout] = []
     
+    private func setMessage(_ message: String) {
+        DispatchQueue.main.async {
+            self.messageLabel.text = message
+        }
+    }
+    
     private func setupHealthStore() {
         hkstore = HKHealthStore()
         
@@ -23,30 +29,20 @@ class ViewController: UIViewController {
                          HKObjectType.quantityType(forIdentifier: HKQuantityTypeIdentifier.heartRate)!,
                          
                          HKObjectType.characteristicType(forIdentifier: HKCharacteristicTypeIdentifier.bloodType)!,
-            ])
+                         ])
         hkstore.requestAuthorization(toShare: [], read: types) {
             (success, error) in
             if !success {
-                DispatchQueue.main.async {
-                    var messageText = "Couldn't access health store."
-                    
-                    if let error = error {
-                        messageText += " \(error)"
-                    }
+                var messageText = "Couldn't access health store."
                 
-                    self.messageLabel.text = messageText
+                if let error = error {
+                    messageText += " \(error)"
                 }
+                self.setMessage(messageText)
+                
             } else {
-                DispatchQueue.main.async {
-                    self.messageLabel.text = "YAY"
-                }
+                self.setMessage("YAY")
             }
-        }
-    }
-    
-    private func setMessage(_ message: String) {
-        DispatchQueue.main.async {
-            self.messageLabel.text = message
         }
     }
     
@@ -67,9 +63,7 @@ class ViewController: UIViewController {
                                   sortDescriptors: nil)
         { (query, samples, error) in
             guard let samples = samples else {
-                DispatchQueue.main.async {
-                    self.messageLabel.text = "so sad"
-                }
+                self.setMessage("so sad")
                 return
             }
             
@@ -82,16 +76,16 @@ class ViewController: UIViewController {
     @IBAction func getCharacteristicData() {
         do {
             let bloodtype = try hkstore.bloodType()
-            self.messageLabel.text = bloodtype.bloodType.humanReadableName()
+            setMessage(bloodtype.bloodType.humanReadableName())
         } catch {
-            self.messageLabel.text = "Could not get blood type: \(error)"
+            setMessage("Could not get blood type: \(error)")
         }
 
     }
 
     override func viewDidLoad() {
         guard HKHealthStore.isHealthDataAvailable() else {
-            messageLabel.text = "OH NOES no health data available!"
+            setMessage("OH NOES no health data available!")
             return
         }
         
